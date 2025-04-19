8b70b33d91d3d3d04d21fce4c826061d71a631c0
PMJ000 <qkralswp789@naver.com>
2025-04-19 12:36:32 +0900

    Dialog Study
    원 찍기, 화면 분할

--- gSolution/MFCApplication1/CDlgImage.h
+++ gSolution/MFCApplication1/CDlgImage.h
@@ -12,7 +12,7 @@ public:
 	CDlgImage(CWnd* pParent = nullptr);   // 표준 생성자입니다.
 	virtual ~CDlgImage();
 
-
+	CImage m_image;
 	CWnd* m_pParent;
 // 대화 상자 데이터입니다.
 #ifdef AFX_DESIGN_TIME
@@ -25,4 +25,7 @@ protected:
 	DECLARE_MESSAGE_MAP()
 public:
 	afx_msg void OnBnClickedBtnUpParent();
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	void InitImage();
 };
